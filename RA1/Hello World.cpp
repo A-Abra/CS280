809ca57fb7691521c20ebe1a20eb5f0213bf5a7e
A-Abra <100457271+A-Abra@users.noreply.github.com>
2023-05-08 12:52:37 -0400

    Add files via upload

--- RA1/Hello World.cpp
+++ RA1/Hello World.cpp
@@ -1,1 +1,11 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+	string name;
+	cout << "Welcome to CS 280." << endl;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Hello, " << name;
+	return 0;
+}
